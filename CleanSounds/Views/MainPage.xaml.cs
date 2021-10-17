ae9ae8cd41ea2fbee10963fc217004ed26ceafe1
ROBERT DE JESUS GENAO MOLINA <1071499@est.intec.edu.do>
2021-10-17 16:51:47 +0800

    Add project files.

---
+++ CleanSounds/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CleanSounds.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void GoToHotelPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HotelPage(), true);
+        }
+    }
+}
